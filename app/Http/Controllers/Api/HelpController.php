a5c145aaf3c807295d741e150d97a0a71bc2a89d
robin hood <fordarnold@gmail.com>
2015-03-05 01:54:04 +0300

    Added routes to API controllers

--- app/Http/Controllers/Api/HelpController.php
+++ app/Http/Controllers/Api/HelpController.php
@@ -1,7 +1,12 @@
-<?php namespace ApplicationName\Http\Controllers;
+<?php
 
 class HelpController extends \BaseController
 {
+	public function getUserAuth()
+  {
+    return View::make('api.help.user_auth');
+  }
+
   public function getResourceDocs($resource)
   {
     $view_path = 'api.help.'.strtolower($resource).'_resource';
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,3 +24,28 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+
+/**
+ * REST API Routes
+ * @author Arnold Mwumva <fordarnold@gmail.com>
+ */
+
+# api help
+Route::get('api/help/users/auth', 'HelpController@getUserAuth');    
+Route::get('api/help/resources/{resource}', 'HelpController@getResourceDocs');
+
+# users
+Route::resource('users','Api\UsersController');
+
+# profiles
+Route::resource('profiles','Api\ProfilesController');
+
+# groups
+Route::resource('groups','Api\GroupsController');
+
+# countries
+Route::resource('countries','Api\CountriesController');
+
+# currencies
+Route::resource('currencies','Api\CurrenciesController');
